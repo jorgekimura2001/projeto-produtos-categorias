40c20aa44ce9bb591dd604c9d32a769bc3dbc9ca
Jorge Kimura <kimurajorge2001@gmail.com>
2022-10-21 11:42:20 -0300

    feat: docker implementado

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use('/categories', categoriesRouter)
 app.use('/products', productsRouter)
 
-app.listen(3333, () => {
+app.listen(process.env.PORT, () => {
   startDatabase()
 });
 
